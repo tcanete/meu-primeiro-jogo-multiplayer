0b067a67c30416875096e2182355e2442e77e71b
Thiago Henrique Freitas Canete <thiago.canete@gmail.com>
2020-03-31 22:30:35 +0000

    versão vídeo final

---
+++ playground/tcanete-1st-release/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/game.js'
+import socketio from 'socket.io'
+
+const app = express();
+const server = http.createServer(app);
+const sockets = socketio(server);
+
+app.use(express.static('public'));
+
+const game = createGame();
+game.start();
+
+game.subscribe((command) => {
+    console.log(`Emitting ${command.type}`);
+    sockets.emit(command.type, command);
+});
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
+
+    game.addPlayer({playerId: playerId});
+    socket.emit('setup', game.state);
+
+    socket.on('disconnect', () => {
+        game.removePlayer({playerId: playerId});
+    });
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId;
+        command.type = 'move-player';
+
+        game.movePlayer(command);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('> Server is listening on port: 3000');
+})
